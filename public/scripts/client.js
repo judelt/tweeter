dbbbd6997c63d9755ff76387b6b18382b3520339
Judit Mendez Puig <mendez.puig@gmail.com>
2021-02-25 17:52:28 +0000

    before responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,128 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const renderTweets = function (data) {
+    for(const tweet of data) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").prepend($tweet); 
+    }
+  };
+
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+
+    const form = $( this );
+    const url = form.attr('action');
+    const serializedData = form.serialize();
+    //const text = $( form ).find('#tweet-text').val()
+
+    $(this).validate({
+      rules: {
+        text: {
+          required: true,
+          minlength: 1,
+          maxlength: 140,
+        }
+      },
+      messages: {
+        text: {
+          required: `<i class="fas fa-exclamation-triangle"></i> Minimum lenght of 1 character`,
+          maxlength: `<i class="fas fa-exclamation-triangle"></i> Maximum message length exceeded`
+        }
+      }
+      //add that to place error message on top
+      // errorElement : 'span',
+      // errorLabelContainer: '#error'
+      
+    });
+
+    // if(!text) {
+    //   let errorMessage = `
+    //   <label id="validation">
+    //     <i class="fas fa-exclamation-triangle"></i> Invalid input
+    //   </label>`;
+      
+    //   //$("new-tweet").append(errorMessage); 
+    //   return $("error").data(errorMessage);
+    // }
+    // if(text.length > 140) {
+    //   let errorMessage = `
+    //   <label id="validation">
+    //     <i class="fas fa-exclamation-triangle"></i> Maximum message length exceeded
+    //   </label>`;
+    //   //$("new-tweet").append(errorMessage);
+      
+    //   //document.getElementById('error').innerHTML = `<i class="fas fa-exclamation-triangle"></i> `;
+    //   return $("error").data(errorMessage);
+    // }
+
+    $.ajax({
+      method: "POST",
+      url: url,
+      data: serializedData // serializes the form's elements.
+    }).then(function () {
+      loadTweets();
+      $(".counter").val(140);
+
+    }).catch (function (err) {
+      console.log(err);
+    })
+
+    document.querySelector("form").reset();
+  
+  });
+
+  const loadTweets = function (){
+    return $.ajax({
+      method: "GET",
+      url: '/tweets',
+    }).then(function (response) {
+    renderTweets(response);
+    }).catch (function (err) {
+      console.log(err);
+    })
+  };
+
+  loadTweets();
+
+});
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+const createTweetElement = function (data) {
+  const a = moment.unix(data.created_at / 1000);
+  const b = moment();
+  const days = b.diff(a, "days"); // =1
+  
+
+  let $tweet = `
+    <article class="tweet">
+      
+      <header>
+        <div><img src="${escape(data.user.avatars)}"><span>${data.user.name}</span></div>
+        <p><strong class="account">${escape(data.user.handle)}</strong></p>
+      </header>
+
+      <footer>
+        <p><strong>${escape(data.content.text)}</strong></p>
+      </footer>
+      <div class="log">
+        <p>${escape(days)} days ago </p>
+      
+        <div>
+          <i class="fab fa-font-awesome-flag fa-xs"></i>
+          <i class="fas fa-retweet fa-xs"></i>
+          <i class="fas fa-heart fa-xs"></i>
+      
+        </div>
+      </div>
+    </article>
+    `;
+  return $tweet;
+};
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $("#tweet-text").on('keyup', tweetCharacterCounter);
+
+});
+
+  const tweetCharacterCounter = function() {
+    let tweetLength = $("#tweet-text").val().length;
+    let counter = $(this).parent().find(".counter");
+    let letterCounting = 140 - tweetLength;
+
+    $(counter).html(letterCounting);
+
+    if(letterCounting  < 0) {
+      $(counter).addClass( "red" );
+    } else {
+      $(counter).removeClass( "red" )
+    }    
+  }
