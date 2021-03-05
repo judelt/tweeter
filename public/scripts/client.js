324a97026833feb0eb27e2fac834b8cd5b312dd7
Judit Mendez Puig <mendez.puig@gmail.com>
2021-03-05 03:47:50 +0000

    fix bug character counting reset & tweet submission(no more duplication)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,73 +4,83 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  const renderTweets = function(data) {
-    for (const tweet of data) {
-      const $tweet = createTweetElement(tweet);
-      $("#tweets-container").prepend($tweet);
-    }
-  };
-
-  $("form").on("submit", function(event) {
+$(document).ready(function () {
+  $("form").on("submit", function (event) {
     event.preventDefault();
-  
-    const form = $(this);
-    const url = form.attr('action');
-    const serializedData = form.serialize();
-    let text = $('#tweet-text').val();
 
-    // Validate tweet is valid
+    const form = $(this);
+    const url = form.attr("action");
+    const serializedData = form.serialize();
+    let text = $("#tweet-text").val();
+
+    // Validate tweet
     if (!validateTweet(text)) {
       return;
     }
-  
+
     $.ajax({
       method: "POST",
       url: url,
-      data: serializedData 
-    }).then(function() {
-      loadTweets();
-      $(".counter").val(140);
-      document.querySelector("form").reset();
-    }).catch(function(err) {
-      console.log(err);
-    });
+      data: serializedData,
+    })
+      .then(function () {
+        //Tweeter feed is emptied before loading tweets again(preventing dupliction)
+        $("#tweets-container").empty();
+        loadTweets();
+
+        //Character counter resets once a tweet is submitted
+        $(".counter").text(140);
+
+        //Form resets once a tweet is submitted
+        document.querySelector("form").reset();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   });
 
-  const loadTweets = function() {
-    return $.ajax({
-      method: "GET",
-      url: '/tweets',
-    }).then(function(response) {
-      renderTweets(response);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  };
-
   loadTweets();
-
 });
 
 //Preventing XSS
-const escape =  function(str) {
-  let div = document.createElement('div');
+const escape = function (str) {
+  let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-const createTweetElement = function(data) {
+const loadTweets = function () {
+  return $.ajax({
+    method: "GET",
+    url: "/tweets",
+  })
+    .then(function (response) {
+      renderTweets(response);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+};
+
+const renderTweets = function (data) {
+  for (const tweet of data) {
+    const $tweet = createTweetElement(tweet);
+    $("#tweets-container").prepend($tweet);
+  }
+};
+
+const createTweetElement = function (data) {
   const a = moment.unix(data.created_at / 1000);
   const b = moment();
   const days = b.diff(a, "days");
-  
 
   let $tweet = `
     <article class="tweet">
       
       <header>
-        <div class="user"><img src="${escape(data.user.avatars)}"><span>${data.user.name}</span></div>
+        <div class="user"><img src="${escape(data.user.avatars)}"><span>${
+    data.user.name
+  }</span></div>
         <p><strong class="account">${escape(data.user.handle)}</strong></p>
       </header>
 
